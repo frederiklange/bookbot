c4c06aa036cd4dd0cd5c00a11bc197c0eefcef0b
frederik.lange@hotmail.com <frederik.lange@hotmail.com>
2024-04-09 23:19:01 +0200

    update python script with letter count function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     word_count = get_words(book_text)
-    print(word_count)
+    letter_count = get_letters(book_text)
+    print(letter_count)
 
 def get_book_text(path):
     with open(path) as f:
@@ -11,5 +12,15 @@ def get_book_text(path):
 def get_words(text):
     words = text.split()
     return len(words)
+
+def get_letters(text):
+    character_count = {}
+    for c in text:    
+        c_lowered = c.lower()
+        if c_lowered in character_count:
+            character_count[c_lowered] += 1
+        else:
+            character_count[c_lowered] = 1
+    return character_count
 
 main()
