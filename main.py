982a9899df1f7e73a9d82366dcf9870d5a486b8e
frederik.lange@hotmail.com <frederik.lange@hotmail.com>
2024-04-09 19:11:24 +0200

    update python script with word count function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-with open("books/frankenstein.txt") as f:
-    text = f.read()
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    word_count = get_words(book_text)
+    print(word_count)
 
-print(text)
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def get_words(text):
+    words = text.split()
+    return len(words)
+
+main()
